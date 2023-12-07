ccd05dd3014af385484dec3377ad2664735867c6
Mirrich:D <MirrichWangD@foxmail.com>
2023-12-07 20:58:07 +0800

    重构项目结构

--- 数值分析/demo.py
+++ 数值分析/demo.py
@@ -1,106 +1,89 @@
-import math
+# -*- coding: utf-8 -*-
+"""
++++++++++++++++++++++++++++++++++++
+@ File        : demo.py
+@ Time        : 2023/11/30 13:32:32
+@ Author      : Mirrich Wang
+@ Version     : Python 3.8.12 (Conda)
++++++++++++++++++++++++++++++++++++
+True LLA: (22.13484127150639, 113.5409844704088)
+Only use latitude and longitude to complete task.
+WGS84 to ECEF is using pyproj module
++++++++++++++++++++++++++++++++++++
+"""
+
+import warnings
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pyproj import Transformer
+
+warnings.filterwarnings("ignore")
+
+"""=========================
+@@@ Initialize Transformer
+========================="""
+
+wgs2utm = Transformer.from_crs("epsg:4326", "epsg:32649")
+utm2wgs = Transformer.from_crs("epsg:32649", "epsg:4326")
+
+"""==============================
+@@@ Data Loading and Preprossing
+=============================="""
+
+data = pd.read_csv("./data/GPSdata1206.csv")
+data = data.drop_duplicates()  # drop duplicates
+
+latitude = data["latitude"].to_numpy()
+longitude = data["longitude"].to_numpy()
+altitude = data["altitude"].to_numpy()
+accuracy = data["accuracy"].to_numpy()
+
+# wgs84 to utm (x, y)
+x_lst, y_lst = [], []
+for lat, lon in zip(latitude, longitude):
+    x, y = wgs2utm.transform(lat, lon)
+    x_lst.append(x)
+    y_lst.append(y)
+
+# convert to array
+xx = np.array(x_lst)
+yy = np.array(y_lst)
+d = accuracy
 
 
-a = 6378137.0
-f = 1 / 298.257223565
+# Min-Max standardization
+x_min, x_max = xx.min(), xx.max()
+y_min, y_max = yy.min(), yy.max()
+d_min, d_max = d.min(), d.max()
+
+x = (xx - x_min) / (x_max - x_min)
+y = (yy - y_min) / (y_max - y_min)
+d = (d - d_min) / (d_max - d_min)
 
 
-# latitude:纬度 longitude:经度 altitude:海拔
-def LLA_to_XYZ(latitude, longitude, altitude):
-    # 经纬度的余弦值
-    cosLat = math.cos(latitude * math.pi / 180)
-    sinLat = math.sin(latitude * math.pi / 180)
-    cosLon = math.cos(longitude * math.pi / 180)
-    sinLon = math.sin(longitude * math.pi / 180)
-
-    # WGS84坐标系的参数
-    rad = 6378137.0  # 地球赤道平均半径（椭球长半轴：a）
-    f = 1.0 / 298.257224  # WGS84椭球扁率 :f = (a-b)/a
-    C = 1.0 / math.sqrt(cosLat * cosLat + (1 - f) * (1 - f) * sinLat * sinLat)
-    S = (1 - f) * (1 - f) * C
-    h = altitude
-
-    # 计算XYZ坐标
-    X = (rad * C + h) * cosLat * cosLon
-    Y = (rad * C + h) * cosLat * sinLon
-    Z = (rad * S + h) * sinLat
-
-    N = a / np.sqrt(1 - f * (2 - f) * sinLat**2)
-
-    X = (N + altitude) * cosLat * cosLon
-    Y = (N + altitude) * cosLat * sinLon
-    Z = (N * (1 - f) ** 2 + altitude) * sinLat
-
-    return np.array([X, Y, Z])
+"""======================
+@@@ Least Squares Method
+======================"""
 
 
-def XYZ_to_LLA(X, Y, Z):
-    # WGS84坐标系的参数
-    a = 6378137.0  # 椭球长半轴
-    b = 6356752.314245  # 椭球短半轴
-    ea = np.sqrt((a**2 - b**2) / a**2)
-    eb = np.sqrt((a**2 - b**2) / b**2)
-    p = np.sqrt(X**2 + Y**2)
-    theta = np.arctan2(Z * a, p * b)
+A = 2 * np.concatenate([[x[1:] - x[:-1]], [y[1:] - y[:-1]]], axis=0).T
 
-    # 计算经纬度及海拔
-    longitude = np.arctan2(Y, X)
-    latitude = np.arctan2(Z + eb**2 * b * np.sin(theta) ** 3, p - ea**2 * a * np.cos(theta) ** 3)
-    N = a / np.sqrt(1 - ea**2 * np.sin(latitude) ** 2)
-    altitude = p / np.cos(latitude) - N
+B = d[:-1] ** 2 - d[1:] ** 2 - (x[:-1] ** 2 + y[:-1] ** 2) + (x[1:] ** 2 + y[1:] ** 2)
 
-    return np.array([np.degrees(latitude), np.degrees(longitude), altitude])
+# solution
+X = np.linalg.inv(A.T @ A) @ (A.T @ B)
 
+# restore ture x, y, z
+x_pred = X[0] * (x_max - x_min) + x_min
+y_pred = X[1] * (y_max - y_min) + y_min
 
-# 根据经度、纬度、海拔计算两地距离:法一
-def get_distance1(position1, position2):
-    v1 = LLA_to_XYZ(position1[0], position1[1], position1[2])
-    v2 = LLA_to_XYZ(position2[0], position2[1], position2[2])
-    distance = np.linalg.norm(v1 - v2)
+# restore latitude, longitude
+lat_pred, lon_pred = utm2wgs.transform(x_pred, y_pred)
 
-    return distance
+print("x_pred: %f, y_pred: %f" % (x_pred, y_pred))
+print("lat_pred: %f, lon_pred: %f" % (lat_pred, lon_pred))
 
-
-# 根据经度、纬度计算两地距离：法二
-def get_distance2(lat1, lon1, lat2, lon2):
-    """获取地理坐标系下的两点间距离"""
-    # GetDistanceInGeographyCoordinate, return two point distance
-    radius_lat1 = lat1 * math.pi / 180
-    radius_lat2 = lat2 * math.pi / 180
-    radius_lon1 = lon1 * math.pi / 180
-    radius_lon2 = lon2 * math.pi / 180
-    a = radius_lat1 - radius_lat2
-    b = radius_lon1 - radius_lon2
-    distance = 2 * math.asin(
-        math.sqrt(pow(math.sin(a / 2.0), 2) + math.cos(radius_lat1) * math.cos(radius_lat2) * pow(math.sin(b / 2.0), 2))
-    )
-
-    distance = distance * 6378137
-    distance = distance - (distance * 0.0011194)
-    return distance
-
-
-def get_bearing(lat1, lon1, lat2, lon2):
-    dlon = lon2 - lon1
-
-    y = math.sin(dlon) * math.cos(lat2)
-    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dlon)
-
-    bearing = math.atan2(y, x)
-    bearing = np.degrees(bearing)
-    bearing = (bearing + 360) % 360
-
-    return bearing
-
-
-latitude = 22.138214
-longitude = 113.538533
-altitude = 37.271128
-
-
-x, y, z = LLA_to_XYZ(latitude, longitude, altitude)
-print(x, y, z)
-
-lat, lon, alt = XYZ_to_LLA(x, y, z)
-print(lat, lon, alt)
+plt.plot(longitude, latitude, "b*")
+plt.plot(lon_pred, lat_pred, "r.")
+plt.show()
